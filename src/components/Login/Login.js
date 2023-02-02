d93dbd26256316fa458b2cd8f160685f42710485
Hamsa Aldrobi <hamsa.aldrobi@infinitaslearning.com>
2023-02-02 18:28:18 +0100

    refactor: use central context manager extracting the main common functions from the provider App

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -48,12 +49,14 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
     isValid: undefined,
-  })
+  });
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: undefined,
-  })
+  });
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -90,7 +93,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
